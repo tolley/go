c6d6bce9c3bece7799beaf0bd6d7a5719fd23f81
Tolley <chris.tolley@gmail.com>
2009-10-15 00:23:52 -0400

    Gave the rendered board a fixed size, and made parsertest more user friendly

--- parsertest.php
+++ parsertest.php
@@ -15,34 +15,54 @@
 
 $( document ).ready( function()
 {
-	// The url of the sgf file data
-	var url = '/handicap.sgf';
-
-	// Create the board image
-	var goban = $( '#goban' ).gogame( { 'chatWindow': '#chatWindow', 
-					    'url': url, 
-					    'format': 'sgf',
-					    'nextBtn': '#next',
-					    'previousBtn': '#previous' } );
+	$( '#goban' ).gogame( { 'url': '/handicap.sgf', 
+				'format': 'sgf',
+				'nextBtn': '#next',
+				'previousBtn': '#previous',
+				'chatWindow': '#chatWindow' } );
 } );
-
 </script>
 
 </head>
 
 <body>
 
-<div id="goban">
+<div style="width: 900px;">
+	<div style="float:right;">
+	
+	<h2>What am I looking at?</h2>
+	
+	<p style="width:400px;">
+	This is a demo page for a go board plugin for jQuery.  This plugin will let users
+	specify a saved game file on the server, and a flag to indicate the file format.
+	It will fetch the file from the server, parse it into javascript objects, and provide 
+	input options that will allow the user to step back and forth through the game.
+	</p>
+
+	<div style="">Here's what the page javascript looks like:</div>
+	<pre>
+$( document ).ready( function()
+{
+	$( '#goban' ).gogame( { 'url': '/handicap.sgf', 
+				'format': 'sgf',
+				'nextBtn': '#next',
+				'previousBtn': '#previous',
+				'chatWindow': '#chatWindow' } );
+} );
+	</pre>
+	
+	</div>
+	
+	<div id="goban">
+	</div>
+	
+	<textarea id="chatWindow" style="width:382px; height:100px;"></textarea>
+	<br />
+	
+	<button id="previous">Previous</button>
+	<button id="next">Next</button>
+	<br />
 </div>
-
-<br />
-
-<textarea id="chatWindow" style="width:400px; height:150px;"></textarea>
-<br />
-
-<button id="next">Next</button>
-<button id="previous">Previous</button>
-<br />
 
 </body>
 
